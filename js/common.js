d9d815aa938575e8f3bf0ea203bf4f002717d2de
qianduan88 <qiandaun2018@163.com>
2018-08-01 12:35:11 +0800

    修改
    修改

--- js/common.js
+++ js/common.js
@@ -2,4 +2,17 @@ $(function(){
     //静态加载
     $("#head").load('public/header.html');
     $("#footer").load('public/footer.html');
+
+    $(".headerRight a:contains('登录')").click(function(){
+        $(".mask").show();
+        $(".loginBox").show();
+    })
+    
+    $(".close").click(function(){
+        $(".mask").hide();
+        $(".loginBox").hide();
+    })
+
+
 })
+
